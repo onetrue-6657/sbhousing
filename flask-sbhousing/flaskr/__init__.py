1cfab2dc68d4b1ef89bb521d926d81ae332ba2dd
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-19 18:41:52 -0500

    created initial flask framework

---
+++ flask-sbhousing/flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/sbhousing')
+    def sbhousing():
+        return "我的青春是Morrill Tower，心中永远的第一，4000美元一学期，23层楼，16人套间，4个四人间，3个马桶，2个浴室，2个洗衣机，0次跳楼"
+
+    from . import db
+    db.init_app(app)
+
+    return app
