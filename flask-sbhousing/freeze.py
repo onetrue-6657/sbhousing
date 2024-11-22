45028443d20ad368c6af14d84c1271432a540445
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-22 00:15:47 -0500

    jekyll caonima

--- flask-sbhousing/freeze.py
+++ flask-sbhousing/freeze.py
@@ -5,22 +5,23 @@ app = create_app()
 freezer = Freezer(app)
 
 @freezer.register_generator
-def skip_dynamic_routes():
-    yield '/auth/logout'
+def generate_routes():
+    # 静态页面的路由
+    yield '/'
+    yield '/auth/register/'
+    yield '/auth/login/'
+    yield '/create/'
 
 @freezer.register_generator
-def static_routes():
-    yield '/auth/register'
-    yield '/auth/login'
-    yield '/auth/logout'
-    yield '/create'
-    for post_id in range(1, 1001):
-        yield f'/{post_id}/update'
-
-@freezer.register_generator
-def update_routes():
+def dynamic_routes():
     for post_id in range(1, 1001):
         yield 'blog.update', {'id': post_id}
 
+@freezer.register_generator
+def skip_dynamic_routes():
+    yield '/auth/logout/'
+
 if __name__ == '__main__':
+    for url in freezer.freeze_yield():
+        print(f"Freezing URL: {url}")  # 打印每个生成的 URL
     freezer.freeze()
