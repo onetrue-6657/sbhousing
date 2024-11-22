6529601ceec92593ec8ddcace816bd2eaaf59485
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-22 00:08:01 -0500

    Deploy static site to GitHub Pages

--- flask-sbhousing/flaskr/auth.py
+++ flask-sbhousing/flaskr/auth.py
@@ -9,7 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix = '/auth')
 
-@bp.route('/register', methods = ('GET', 'POST'))
+@bp.route('/register/', methods = ('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -38,7 +38,7 @@ def register():
 
     return render_template('/auth/register.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -74,7 +74,7 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
-@bp.route('/logout')
+@bp.route('/logout/')
 def logout():
     session.clear()
     return redirect(url_for('blog.index'))
--- flask-sbhousing/flaskr/blog.py
+++ flask-sbhousing/flaskr/blog.py
@@ -18,7 +18,7 @@ def index():
     ).fetchall()
     return render_template('/blog/index.html', posts = posts)
 
-@bp.route('/create', methods = ('GET', 'POST'))
+@bp.route('/create/', methods = ('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
@@ -59,7 +59,7 @@ def get_post(id, check_author = True):
 
     return post
 
-@bp.route('/<int:id>/update', methods = ('GET', 'POST'))
+@bp.route('/<int:id>/update/', methods = ('GET', 'POST'))
 @login_required
 def update(id):
     post = get_post(id)
@@ -86,7 +86,7 @@ def update(id):
 
     return render_template('/blog/update.html', post = post)
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<int:id>/delete/', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
